7c604f72560d27e35339b03aac161996e2c35ccd
Chindurex <chinnaracha@gmail.com>
2026-03-02 21:43:01 +0700

    add data ex-1and2

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,16 @@
 //Exercise #1: Variable Creation
 //Start coding here
 
+const fristName = "Karter" ;
+const lastName = "Anderson" ;
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restauront = "Chef's Table by Chef James";
+let bill = 15000;
+let totalOpenRestaurant = 1500 ;
+
+
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+bill = 5000;
+totalOpenRestaurant = 2500;
