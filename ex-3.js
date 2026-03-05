71930ce1f331253850c5261b241722620bd60e9a
ChinDurx <chinnaracha@gmail.com>
2026-03-05 10:22:35 +0700

    Update ex-3.js

--- ex-3.js
+++ ex-3.js
@@ -1,9 +1,9 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
-let fahrenheit = (celsius x 1.8) + 32;
+let fahrenheit = (celsius * 1.8) + 32;
 
 
 
 // Start coding here;
 
-let fahrenheit = (celsius x 1.8) + 32;
+let fahrenheit = (celsius * 1.8) + 32;
